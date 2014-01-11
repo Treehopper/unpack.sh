cc8e9e47d7ef1c736e9b863979a91873bfcacf06
Treehopper <github.com@hohenegger.eu>
2014-01-11 16:12:45 +0100

    Update package.sh

--- packaging/package.sh
+++ packaging/package.sh
@@ -2,12 +2,13 @@
 
 VERSION="1.0-1"
 PACKAGE_FILE="unpack_sh-$VERSION.deb"
+BASE="debian"
 
-cp ../unpack ../pack debian/usr/bin
-dpkg-deb --build debian "unpack_sh-$VERSION.deb"
+cp ../unpack ../pack "$BASE/usr/bin"
+dpkg-deb --build "$BASE" "$PACKAGE_FILE"
 
 echo "Install with dpkg -i $PACKAGE_FILE"
 
-rm debian/usr/bin/*
+rm "$BASE/usr/bin"
 
 exit 0
